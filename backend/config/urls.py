00c0e3964904ef7f722379642f1c7d26edd82610
Maado <mahmoud.dahma@sylndr.com>
2026-05-27 12:34:32 +0200

    disable Django admin panel

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    #path("admin/", admin.site.urls),
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/v1/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/v1/auth/", include("apps.users.urls")),
